979c6e02b8ad50a0190b9af3f74fa5c4f5d22cfa
Levi Bernardo <levibernardo01@gmail.com>
2026-09-18 09:55:46 -0300

    feat: criar base relacional de materias-primas

---
+++ backend/models/__init__.py
@@ -0,0 +1,13 @@
+from models.materia_prima import (
+    ComposicaoMateriaPrima,
+    MateriaPrima,
+    Nutriente,
+    PrecoMateriaPrima,
+)
+
+__all__ = [
+    "ComposicaoMateriaPrima",
+    "MateriaPrima",
+    "Nutriente",
+    "PrecoMateriaPrima",
+]
