f120de2745f8565216c61f8252cc5937c859e2b4
Levi Bernardo <levibernardo01@gmail.com>
2026-09-18 11:20:24 -0300

    feat: calcula custo por apresentação

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,4 +1,5 @@
 from routers.materias_primas import router as materias_primas_router
 from routers.projetos import router as projetos_router
+from routers.apresentacoes import router as apresentacoes_router
 
-__all__ = ["materias_primas_router", "projetos_router"]
+__all__ = ["apresentacoes_router", "materias_primas_router", "projetos_router"]
