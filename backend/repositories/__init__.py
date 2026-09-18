3e41e13cb3f7a67d7c54b8c4f85183d2566c394f
Levi Bernardo <levibernardo01@gmail.com>
2026-09-18 11:20:15 -0300

    feat: calcula custo por apresentação

--- backend/repositories/__init__.py
+++ backend/repositories/__init__.py
@@ -1,4 +1,5 @@
 from repositories.materia_prima_repository import MateriaPrimaRepository
 from repositories.projeto_repository import ProjetoRepository
+from repositories.apresentacao_repository import ApresentacaoRepository
 
-__all__ = ["MateriaPrimaRepository", "ProjetoRepository"]
+__all__ = ["ApresentacaoRepository", "MateriaPrimaRepository", "ProjetoRepository"]
