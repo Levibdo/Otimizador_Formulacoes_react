48a333972a5c248270146fdaf63f87b46333d410
Levi Bernardo <levibernardo01@gmail.com>
2026-09-18 11:01:21 -0300

    feat: adiciona projetos e versões de fórmulas

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,3 +1,4 @@
 from routers.materias_primas import router as materias_primas_router
+from routers.projetos import router as projetos_router
 
-__all__ = ["materias_primas_router"]
+__all__ = ["materias_primas_router", "projetos_router"]
